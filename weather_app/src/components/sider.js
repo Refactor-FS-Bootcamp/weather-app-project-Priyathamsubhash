b8f0d7b0311f62716e5aa7bb7f43ea787341fde0
priyathamsubhash <114746201+Priyathamsubhash@users.noreply.github.com>
2022-11-29 13:24:50 +0530

    weather_app_task2 completed

--- weather_app/src/components/sider.js
+++ weather_app/src/components/sider.js
@@ -1,14 +1,35 @@
-const Sider = ({sider_data}) => {
+import Button from "./button";
+import {useState} from "react"
+const Sider = () => {
+  const [initial,setstyle]=useState('home')
+    const style={
+      styleHome:{
+        backgroundColor:initial==='home'?'orange':'',
+        color:initial==='home'?'blue':''
+      },
+      styleCity:{
+        backgroundColor:initial==='cities'?'orange':'',
+        color:initial==='cities'?'blue':''
+      }
+    }
+    const homeClk=(e)=>{
+
+      e.preventDefault();
+
+      return setstyle('home')
+    }
+    const CityClk=(e)=>{
+
+      e.preventDefault();
+
+      return setstyle('cities')
+    }
     return (
       <div className="sider">
-        {sider_data.map((item) => {
-        return (
-          <div className="sider-content">
-            {item.Name}
-            <hr />
-          </div>
-        );
-      })}
+      
+      <a href="/"><Button buttonname="Home" class="Home" style={style.styleHome}  handleClick={homeClk}/></a>
+      <a href="/"><Button buttonname="Cities" class="cities" style={style.styleCity} handleClick={CityClk}/></a>
+ 
       </div>
     );
   };
